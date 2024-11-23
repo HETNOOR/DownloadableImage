93dbc666dfef2ca7de610842e1493f9006fb6803
HETNOOR <daffixipop@gmail.com>
2024-11-23 15:55:04 +0700

    Initial Commit

---
+++ DownloadableImage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DownloadableImage
+//
+//  Created by Максим Герасимов on 23.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
